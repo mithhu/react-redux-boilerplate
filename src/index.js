c1d0c9ba6d78cf501210d245246c0056b1364a7b
Mahamudul Hasan Mithhu <mithhu666@gmail.com>
2019-01-11 19:24:01 +0600

    fixed middleware

--- src/index.js
+++ src/index.js
@@ -2,15 +2,15 @@ import React from "react";
 import ReactDOM from "react-dom";
 import { Provider } from "react-redux";
 import { createStore, applyMiddleware } from "redux";
-import thunk from 'redux-thunk';
+import thunk from "redux-thunk";
 
 import App from "./components/App";
 import reducers from "./reducers";
 
-const store =createStore(reducers, applyMiddleware(thunk))
+const store = createStore(reducers);
 
 ReactDOM.render(
-  <Provider store={(store, applyMiddleWare(thunk))}>
+  <Provider store={(store, applyMiddleware(thunk))}>
     <App />
   </Provider>,
   document.querySelector("#root")
